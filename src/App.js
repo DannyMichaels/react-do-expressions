00c91c072ab155d078ed096f53b50820568dc55d
Daniel Michael <itzdanielmichael@gmail.com>
2021-05-14 22:58:49 -0400

    update App.js, update webpack config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
+/* eslint-disable no-unused-expressions */
 import './App.css';
+import { useState } from 'react';
+
+const BlueComponent = () => (
+  <div style={{ color: 'white', height: '20em', background: 'blue' }}>blue</div>
+);
+const RedComponent = () => (
+  <div style={{ color: 'white', height: '20em', background: 'red' }}>red</div>
+);
+const GreenComponent = () => (
+  <div style={{ color: 'white', height: '20em', background: 'green' }}>
+    green
+  </div>
+);
 
 function App() {
-  let x = do {
-    if (0 == 1) {
-      ('hello');
-    } else {
-      ('bye');
-    }
-  };
+  const [color, setColor] = useState('blue');
 
-  return <div className="App"></div>;
+  // HAML/ERB users are going to love this one...
+  return (
+    <>
+      <div>
+        {do {
+          if (color === 'blue') {
+            <BlueComponent />;
+          } else if (color === 'red') {
+            <RedComponent />;
+          } else if (color === 'green') {
+            <GreenComponent />;
+          }
+        }}
+      </div>
+      <div
+        style={{
+          marginTop: '10px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}>
+        {['blue', 'red', 'green'].map((color) => (
+          <button
+            style={{ width: '100px', marginRight: '20px', color }}
+            onClick={() => setColor(color)}>
+            {color}
+          </button>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
